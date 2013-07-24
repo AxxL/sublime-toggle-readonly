89408469dae5aab1cecee64c0b0e26fbf15a7c69
Axel Schaefer <https://github.com/AxxL>
2013-07-24 13:20:36 +0200

    true and false was False
    true=True and false=False.

--- toggle_readonly.py
+++ toggle_readonly.py
@@ -4,13 +4,13 @@ class ToggleReadonlyCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		if (self.view.is_read_only()):
 			if (int(sublime.version()) > 3000):
-				self.view.set_read_only(false)
+				self.view.set_read_only(False)
 			else:
 				self.view.set_read_only(0)
 			sublime.status_message("Buffer " + str(self.view.file_name()) + " is writeable again.")
 		else:
 			if (int(sublime.version()) > 3000):
-				self.view.set_read_only(true)
+				self.view.set_read_only(True)
 			else:
 				self.view.set_read_only(1)
 			sublime.status_message("Buffer " + str(self.view.file_name()) + " is set readonly.")
